fbf177b7926c1100e6523e56387327731dd8ffb0
Emeka Nwadiogbu, Jr. <emekanwadiogbu.jr@me.com>
2024-11-24 19:08:09 -0700

    setup toggle switch to work with document.setAttribute('data-theme', 'dark')

---
+++ -- template-light-dark-mode/script.js
@@ -0,0 +1,12 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// Switch theme dynamically
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+// Event Listener
+toggleSwitch.addEventListener("change", switchTheme);
